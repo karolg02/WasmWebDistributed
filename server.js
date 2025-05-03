cc83c4d6b0608bbd55a29191592fd49748a17978
karolg02 <karolglanowski02@gmail.com>
2025-05-03 15:40:14 +0200

    +few changes

--- server.js
+++ server.js
@@ -2,20 +2,31 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+let sum = 0;
+
 wss.on("connection", (ws) => {
     console.log("Połączono przez WebSocket");
 
-    const task = {
-        type: "task",
-        taskId: "task001",
-        a: 6,
-        b: 5
-    };
+    const sendTask = () => {
+        const task = {
+            type: "task",
+            taskId: "task_" + Date.now(),
+            a: Math.floor(Math.random() * 10),
+            b: Math.floor(Math.random() * 10)
+        };
+        ws.send(JSON.stringify(task));
+    }
 
-    ws.send(JSON.stringify(task));
+    sendTask();
 
-    ws.on("message", (data) => {
-        console.log("Otrzymano: ", data.toString());
+    ws.on("message", (message) => {
+        console.log("Otrzymano: ", message.toString());
+        const data = JSON.parse(message.toString());
+        if (data.type === "result") {
+            sum += data.result;
+        }
+        console.log("Globalna suma wynosi: " + sum);
+        if (sum < 1000000) sendTask();
     });
 
     ws.on("close", () => {
